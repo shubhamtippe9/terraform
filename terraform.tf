914b45836e0cc1e352f7b98d92ae055811e9ab32
shubham <shubhamtippe9@gmail.com>
2026-03-01 13:28:34 +0530

    third commit

--- terraform.tf
+++ terraform.tf
@@ -1,11 +1,29 @@
-provider "aws" {
-  region = "us-east-1"
+resource "aws_vpc" "example" {
+  cidr_block = "172.16.0.0/16"
+
+  tags = {
+    Name = "my-vpc"
+  }
 }
-resource "aws_instance" "fct" {
-  ami = "ami-0f3caa1cf4417e51b"
-instance_type = "t3.micro"
-count=3
-tags = {
-  Name = "web-server${count.index}"
-} 
+
+resource "aws_subnet" "example" {
+  vpc_id            = aws_vpc.example.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "us-east-2a"
+
+  tags = {
+    Name = "my-subnet"
+  }
+}
+
+resource "aws_instance" "example" {
+  ami           = ami-0f3caa1cf4417e51b
+  instance_type = "t3.micro"
+  subnet_id     = aws_subnet.example.id
+
+  
+
+  tags = {
+    Name = "my-instance"
+  }
 }
